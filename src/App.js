53c99edca394f2407169e379157d852c0b11127e
tejasdhanani <tejas27dhanani@gmail.com>
2023-08-29 07:28:56 -0700

    initialized the movie project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Movie from './components/Movie';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb6f22536d7f1f3fded2cebfa99b5c02&page=1";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=eb6f22536d7f1f3fded2cebfa99b5c02&page=1";
+
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API).then(resp => resp.json())
+      .then(data => {
+        setMovies(data.results);
+      })
+
+
+  }, [])
+
+  return (
+    <div>
+      {movies.map(movie => (
+        <Movie key={movie.id} data={movie} />
+      ))}
+
+    </div>
+  );
+}
+
+export default App;
