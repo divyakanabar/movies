59dd40da923feedcaaf5e16b8a42ec314ff6f5b5
Divya <divyakanabar9@gmail.com>
2023-09-02 16:08:48 -0700

    Added logic and style to Search Button

--- src/App.js
+++ src/App.js
@@ -4,25 +4,49 @@ import Movie from './components/Movie';
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb6f22536d7f1f3fded2cebfa99b5c02&page=1";
 
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=eb6f22536d7f1f3fded2cebfa99b5c02&page=1";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=eb6f22536d7f1f3fded2cebfa99b5c02&query=";
 
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch(FEATURED_API).then(resp => resp.json())
+    fetch(FEATURED_API).then(res => res.json())
       .then(data => {
         setMovies(data.results);
-      })
+      });
 
 
   }, [])
 
+  const handleOnSubmit = (e) =>{
+    e.preventDefault();
+
+    fetch(SEARCH_API+searchTerm).then(res => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+
+    setSearchTerm('');
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
     <>
     <header>
-        <input className="search" type="search" placeholder="Search..." />
+      <form onSubmit={handleOnSubmit}>
+        <input
+          className="search"
+          type="search"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleOnChange}
+        />
+      </form>
     </header>
     <div className="movie-container">
       {movies.map(movie => (
